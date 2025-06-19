f39de54bd6a42e9a0554095c0b4d87d5038db822
sarahmschroeder <schroeder.sarahmaria@gmail.com>
2025-06-19 18:07:38 -0300

    Adiciona o Distributions

--- src/SOpt.jl
+++ src/SOpt.jl
@@ -2,11 +2,13 @@ module SOpt
 
     # Carrega as dependencias
     using LFrame
+    using Distributions
     using LinearAlgebra
     using Test
     using OrderedCollections
     using Lgmsh
     using YAML
+
 
     # Carrega os modulos
     include("main.jl")
